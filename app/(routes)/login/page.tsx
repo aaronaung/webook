8c3f0ef063a0953eca14efa420591ad2123c577f
Aaron Aung <aaronaung.95@gmail.com>
2024-03-22 20:14:17 -0700

    Change copy + fix post login redirect

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 import UserAuthForm from "@/src/components/forms/user-auth-form";
 import { getAuthUser } from "@/src/data/user";
 import { supaServerComponentClient } from "@/src/data/clients/server";
-import { sidebarNavigation } from "../app/business/navigation";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -18,7 +17,7 @@ export default async function LoginPage({
 }) {
   const user = await getAuthUser({ client: supaServerComponentClient() });
   if (user) {
-    redirect(searchParams.return_path || sidebarNavigation[0].href);
+    redirect(searchParams.return_path || "/app/student/classes");
   }
 
   return <UserAuthForm returnPath={searchParams.return_path} />;
